35d578b8d86921601f122e79a24ba228683d4730
MladenSU <mladen.s.uzunov@gmail.com>
2023-10-21 22:29:06 +0100

    Adding it as service to RPI

---
+++ ethernet_check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+console_device="/dev/tty1"
+INTERFACE="eth0" 
+command_to_execute="sudo python3 /root/color-mtr/cmtr-rpi.py -n -i 2 -c 10 google.com -o LSAW -w"
+initial_run=true
+
+function write_to_tty {
+    echo -e "\n" >> $console_device
+    writevt "$console_device" "$1"
+}
+
+while true; do
+    if ip link show "$INTERFACE" | grep -q "state UP"; then
+        if [ "$initial_run" = true ]; then
+            write_to_tty "Ethernet cable is connected."
+            sleep 5
+            script -q -c "$command_to_execute" -f "$console_device"
+            initial_run=false
+            write_to_tty "Done with the report!"
+        fi
+        while ip link show "$INTERFACE" | grep -q "state UP"; do
+            sleep 5
+        done
+        write_to_tty "Ethernet cable is disconnected." && echo -e "\n" >> $console_device
+        initial_run=true
+    fi
+done
